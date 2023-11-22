08baab418b403292c892b4161460b9a924a96c69
mewmewmew444 <marianekaterina2014@gmail.com>
2023-11-22 19:09:54 +1000

    django init app + frontend dir

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from users import views as usersView
 
 urlpatterns = [
-    path('login/', usersView.login, name='login')
+    path('', usersView.login, name='login')
 ]
